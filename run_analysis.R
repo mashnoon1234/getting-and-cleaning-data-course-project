5c78da30136d938e905b4c66024fdbb0afb84859
Mashnoon Islam <mashnoon1234@gmail.com>
2020-06-13 21:05:26 +0600

    merge merged train and merged test sets. extract data of features with keyword "mean" and "std". replace integer activity labels with actual labels.

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,5 @@
+library(dplyr)
+
 X_train = read.table("UCI\ HAR\ Dataset/train/X_train.txt")
 y_train = read.table("UCI\ HAR\ Dataset/train/y_train.txt")
 X_test = read.table("UCI\ HAR\ Dataset/test/X_test.txt")
@@ -5,19 +7,26 @@ y_test = read.table("UCI\ HAR\ Dataset/test/y_test.txt")
 feature_names = read.table("UCI\ HAR\ Dataset/features.txt")
 activity_labels = read.table("UCI\ HAR\ Dataset/activity_labels.txt")
 
-print(paste("X_train ncols: ", ncol(X_train)))
-print(paste("y_train ncols: ", ncol(y_train)))
-print(paste("X_test ncols: ", ncol(X_test)))
-print(paste("y_test ncols: ", ncol(y_test)))
+print(paste("X_train dims: (", dim(X_train)[1], ",", dim(X_train)[2], ")", sep = ""))
+print(paste("y_train dims: (", dim(y_train)[1], ",", dim(y_train)[2], ")", sep = ""))
+print(paste("X_test dims: (", dim(X_test)[1], ",", dim(X_test)[2], ")", sep = ""))
+print(paste("y_test dims: (", dim(y_test)[1], ",", dim(y_test)[2], ")", sep = ""))
 
 merge_X_train_y_train = cbind(X_train, y_train)
-names(merge_X_train_y_train) = feature_names[, "V2"]
-names(merge_X_train_y_train)[562] = "activity_labels"
-print(head(merge_X_train_y_train, n = 1))
-
 merge_X_test_y_test = cbind(X_test, y_test)
-names(merge_X_test_y_test) = feature_names[, "V2"]
-names(merge_X_test_y_test)[562] = "activity_labels"
-print(head(merge_X_train_y_train, n = 1))
+
+merge_train_test = rbind(merge_X_train_y_train, merge_X_test_y_test)
+
+names(merge_train_test) = feature_names[, "V2"]
+names(merge_train_test)[562] = "activity_labels"
+
+print(paste("merge_train_test dims: (", dim(merge_train_test)[1], ",", dim(merge_train_test)[2], ")", sep = ""))
+
+mean_std_measurements = select(merge_train_test, contains("mean") | contains("std") | "activity_labels")
+print(names(mean_std_measurements))
+
+map = setNames(as.vector(activity_labels[, "V2"]), as.vector(activity_labels[, "V1"]))
+mean_std_measurements[, "activity_labels"] = map[mean_std_measurements[, "activity_labels"]]
+print(head(mean_std_measurements))
 
 
