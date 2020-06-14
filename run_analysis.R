703727d78844a9f549e5ce1a33487734b9f25ce4
Mashnoon Islam <mashnoon1234@gmail.com>
2020-06-14 15:46:54 +0600

    add subject to dataset. compute column-wise mean grouped by activity label and subject id.

--- run_analysis.R
+++ run_analysis.R
@@ -5,34 +5,40 @@ if (!require("dplyr", character.only = TRUE)) {
 
 X_train = read.table("UCI\ HAR\ Dataset/train/X_train.txt")
 y_train = read.table("UCI\ HAR\ Dataset/train/y_train.txt")
+subject_train = read.table("UCI\ HAR\ Dataset/train/subject_train.txt")
 X_test = read.table("UCI\ HAR\ Dataset/test/X_test.txt")
 y_test = read.table("UCI\ HAR\ Dataset/test/y_test.txt")
+subject_test = read.table("UCI\ HAR\ Dataset/test/subject_test.txt")
 feature_names = read.table("UCI\ HAR\ Dataset/features.txt")
 activity_labels = read.table("UCI\ HAR\ Dataset/activity_labels.txt")
 
-print(paste("X_train dims: (", dim(X_train)[1], ",", dim(X_train)[2], ")", sep = ""))
-print(paste("y_train dims: (", dim(y_train)[1], ",", dim(y_train)[2], ")", sep = ""))
-print(paste("X_test dims: (", dim(X_test)[1], ",", dim(X_test)[2], ")", sep = ""))
-print(paste("y_test dims: (", dim(y_test)[1], ",", dim(y_test)[2], ")", sep = ""))
+# print(paste("X_train dims: (", dim(X_train)[1], ",", dim(X_train)[2], ")", sep = ""))
+# print(paste("y_train dims: (", dim(y_train)[1], ",", dim(y_train)[2], ")", sep = ""))
+# print(paste("subject_train dims: (", dim(subject_train)[1], ",", dim(subject_train)[2], ")", sep = ""))
+# print(paste("X_test dims: (", dim(X_test)[1], ",", dim(X_test)[2], ")", sep = ""))
+# print(paste("y_test dims: (", dim(y_test)[1], ",", dim(y_test)[2], ")", sep = ""))
+# print(paste("subject_test dims: (", dim(subject_test)[1], ",", dim(subject_test)[2], ")", sep = ""))
 
 merge_X_train_y_train = cbind(X_train, y_train)
+merge_X_train_y_train_subject_train = cbind(merge_X_train_y_train, subject_train)
 merge_X_test_y_test = cbind(X_test, y_test)
+merge_X_test_y_test_subject_test = cbind(merge_X_test_y_test, subject_test)
 
-merge_train_test = rbind(merge_X_train_y_train, merge_X_test_y_test)
+merge_train_test = rbind(merge_X_train_y_train_subject_train, merge_X_test_y_test_subject_test)
 
 names(merge_train_test) = feature_names[, "V2"]
 names(merge_train_test)[562] = "activity_labels"
+names(merge_train_test)[563] = "subject"
 
 print(paste("merge_train_test dims: (", dim(merge_train_test)[1], ",", dim(merge_train_test)[2], ")", sep = ""))
 
-mean_std_measurements = select(merge_train_test, contains("mean") | contains("std") | "activity_labels")
-print(names(mean_std_measurements))
+mean_std_measurements = select(merge_train_test, contains("mean") | contains("std") | "activity_labels" | "subject")
 
 map = setNames(as.vector(activity_labels[, "V2"]), as.vector(activity_labels[, "V1"]))
 mean_std_measurements[, "activity_labels"] = map[mean_std_measurements[, "activity_labels"]]
 print(head(mean_std_measurements))
 
-columnwise_mean = mean_std_measurements %>% group_by(activity_labels) %>%
+columnwise_mean = mean_std_measurements %>% group_by(activity_labels, subject) %>%
     summarise_all(funs(mean(.)))
 
 columnwise_mean = as.data.frame(columnwise_mean)
