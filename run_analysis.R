f7b159e998092704c4d8e42b1e6ad4df41f7cac3
Mashnoon Islam <mashnoon1234@gmail.com>
2020-06-14 17:57:10 +0600

    add description of script at the top of the script.

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,9 @@
+# -------------------- Description --------------------
+# This script installs and loads the required package dependences,
+# loads and merges the train and test data, selects the columns
+# with the keywords "mean" and "std" and writes two tidy datasets
+# to two output files.
+
 if (!require("dplyr", character.only = TRUE)) {
     install.packages("dplyr", dependencies = TRUE)
     library("dplyr", character.only = TRUE)
@@ -12,13 +18,6 @@ subject_test = read.table("UCI\ HAR\ Dataset/test/subject_test.txt")
 feature_names = read.table("UCI\ HAR\ Dataset/features.txt")
 activity_labels = read.table("UCI\ HAR\ Dataset/activity_labels.txt")
 
-# print(paste("X_train dims: (", dim(X_train)[1], ",", dim(X_train)[2], ")", sep = ""))
-# print(paste("y_train dims: (", dim(y_train)[1], ",", dim(y_train)[2], ")", sep = ""))
-# print(paste("subject_train dims: (", dim(subject_train)[1], ",", dim(subject_train)[2], ")", sep = ""))
-# print(paste("X_test dims: (", dim(X_test)[1], ",", dim(X_test)[2], ")", sep = ""))
-# print(paste("y_test dims: (", dim(y_test)[1], ",", dim(y_test)[2], ")", sep = ""))
-# print(paste("subject_test dims: (", dim(subject_test)[1], ",", dim(subject_test)[2], ")", sep = ""))
-
 merge_X_train_y_train = cbind(X_train, y_train)
 merge_X_train_y_train_subject_train = cbind(merge_X_train_y_train, subject_train)
 merge_X_test_y_test = cbind(X_test, y_test)
@@ -30,19 +29,15 @@ names(merge_train_test) = feature_names[, "V2"]
 names(merge_train_test)[562] = "activity_labels"
 names(merge_train_test)[563] = "subject"
 
-print(paste("merge_train_test dims: (", dim(merge_train_test)[1], ",", dim(merge_train_test)[2], ")", sep = ""))
-
 mean_std_measurements = select(merge_train_test, contains("mean") | contains("std") | "activity_labels" | "subject")
 
 map = setNames(as.vector(activity_labels[, "V2"]), as.vector(activity_labels[, "V1"]))
 mean_std_measurements[, "activity_labels"] = map[mean_std_measurements[, "activity_labels"]]
-print(head(mean_std_measurements))
 
-columnwise_mean = mean_std_measurements %>% group_by(activity_labels, subject) %>%
-    summarise_all(funs(mean(.)))
+columnwise_mean = mean_std_measurements %>% 
+    group_by(activity_labels, subject) %>% summarise_all(funs(mean(.)))
 
 columnwise_mean = as.data.frame(columnwise_mean)
-print(columnwise_mean)
 
 write.table(columnwise_mean, "tidy_columnwise_mean.txt")
 write.table(mean_std_measurements, "tidy_mean_std_measurements.txt")
